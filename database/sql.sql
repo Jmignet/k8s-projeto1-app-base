612e278fc01333da4234f6b5074cfbcd9bead366
Jmignet <jmig.kat@gmail.com>
2022-12-21 22:17:20 -0300

    Editing out unused initializations
    Also increasing length of one field

--- database/sql.sql
+++ database/sql.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE meubanco;
+--CREATE DATABASE meubanco;
 
-USE meubanco;
+--USE meubanco;
 
 CREATE TABLE Mensagens(
 	id int,
 	nome varchar [50],
 	email varchar[50],
-	comentario varchar[50]
+	comentario varchar[100]
 )
