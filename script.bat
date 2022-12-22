384f4e70acf773555956f88d4c090c04bf24cc1e
Jmignet <jmig.kat@gmail.com>
2022-12-21 22:51:00 -0300

    Update script.bat
    To include services an deployment

--- script.bat
+++ script.bat
@@ -8,3 +8,11 @@ echo "Fazendo upload das imagens..."
 
 docker push docker-hub-user/k8s-projeto1-app-backend:1.0
 docker push docker-hub-user/k8s-database-app-database:1.0
+
+echo "Criando serviço no cluster k8s..."
+
+kubectl apply -f ./services.yml
+
+echo "Realizando deployment no cluster k8s..."
+
+kubectl apply -f ./deployment.yml
